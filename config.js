d2fdb88fc784db006f28b2be2ba9f6c20f2efafe
HamzaAbro <91449044+HamzaAbro@users.noreply.github.com>
2026-09-13 21:16:34 +0500

    Update GOOGLE_SHEETS_WEB_APP_URL in config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.CAMPAIGN_CONFIG = {
   GOOGLE_SHEETS_WEB_APP_URL:
-    "https://script.google.com/macros/s/AKfycbwt-sUu-t5KKHanFgiZUaP-rqL1Cjw7zcFDxDk_2IdIDBWjkmVsnSqjOSzxpGQ2to3r/exec"
+    "    "https://script.google.com/macros/s/AKfycbxZUg_B_ttozyd4WV8l3OYdwHe51Ihm_Ib5Vjqmw901C8qwTvGjVl6Xnt_YmY3HPDc/exec"
+"
 };
