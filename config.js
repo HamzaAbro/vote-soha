d3c2a47d9e8b64905f2043da1b6ce872646c12e3
HamzaAbro <91449044+HamzaAbro@users.noreply.github.com>
2026-09-13 21:25:48 +0500

    Fix formatting of CAMPAIGN_CONFIG in config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.CAMPAIGN_CONFIG = {
   GOOGLE_SHEETS_WEB_APP_URL:
-    "    "https://script.google.com/macros/s/AKfycbxZUg_B_ttozyd4WV8l3OYdwHe51Ihm_Ib5Vjqmw901C8qwTvGjVl6Xnt_YmY3HPDc/exec"
-"
+    "https://script.google.com/macros/s/AKfycbxZUg_B_ttozyd4WV8l3OYdwHe51Ihm_Ib5Vjqmw901C8qwTvGjVl6Xnt_YmY3HPDc/exec"
 };
